3f05e5cb8233a26fbe5344dca161fe54e3248612
SeptemberHX <september_hx@outlook.com>
2022-05-12 18:01:07 +0800

    update

--- src/common.ts
+++ src/common.ts
@@ -24,3 +24,4 @@ export enum MarkdownViewEvents {
     NoteDidUpdate = 'NoteDidUpdate',
     NewNoteOpen = 'NewNoteOpen',
 }
+
--- src/driver/markdownView/webview/index.ts
+++ src/driver/markdownView/webview/index.ts
@@ -1,4 +1,12 @@
-// import {MarkdownViewEvents} from "../../../common";
+import {AUTO_NOTE_LINK_PLUGIN_ID, MarkdownViewEvents, QueryAllNoteId2TitleRequest} from "../../../common";
+import Dict = NodeJS.Dict;
+
+declare const webviewApi: {
+    postMessage: (
+        id: string,
+        payload: QueryAllNoteId2TitleRequest,
+    ) => Promise<Dict<string>>;
+};
 
 export class MarkdownView {
 
@@ -11,9 +19,32 @@ export class MarkdownView {
         document.addEventListener('joplin-noteDidUpdate', async () => {
             console.log('=====> In joplin-noteDidUpdate event');
             // this.emit(MarkdownViewEvents.NoteDidUpdate);
-        });
+            const currentNoteId2Title = await webviewApi.postMessage(AUTO_NOTE_LINK_PLUGIN_ID, {
+                event: 'queryAllNoteId2Title'
+            });
 
-        console.log('===========================================');
+            console.log(currentNoteId2Title);
+            let titles = [];
+            let title2Id = {}
+            for (let noteId in currentNoteId2Title) {
+                titles.push(currentNoteId2Title[noteId]);
+                title2Id[noteId] = currentNoteId2Title[noteId];
+            }
+            let regStr = titles.join('|');
+            let reg = new RegExp(`(${regStr})`, 'g');
+            console.log(reg);
+            // document.body.innerHTML = document.body.innerHTML.replace(reg, (text) => {
+            //     if (text in title2Id) {
+            //         return `[${text}](:/${title2Id[text]})`;
+            //     } else {
+            //         return text;
+            //     }
+            // });
+
+            // document.body.innerHTML = document.body.innerHTML.replace('测试一下', '不要测试');
+            // console.log(document.body.innerText);
+            // console.log(document.body.innerHTML);
+        });
     }
 }
 
