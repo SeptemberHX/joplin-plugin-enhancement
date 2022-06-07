a91101ac75d881f8997210200d3bdaa066536bf5
SeptemberHX <september_hx@outlook.com>
2022-06-07 22:01:10 +0800

    add: image size {} now is rendered in gray color in editor

--- src/driver/codemirror/admonition/index.ts
+++ src/driver/codemirror/admonition/index.ts
@@ -66,6 +66,32 @@ module.exports = {
 
                 CodeMirror.defineOption("gfm-joplin-markdown", [], async function(cm, val, old) {
                     cm.setOption('mode', 'gfm-joplin-markdown');
+
+                    cm.addOverlay({
+                        requiredSettings: ['extraCSS'],
+                        token: function (stream: any) {
+                            const match = exec(/(?<=(!\[.*]\(.*\)))(\{.*\})/g, stream);
+
+                            const baseToken = stream.baseToken();
+                            if (baseToken?.type && (
+                                baseToken.type.includes("jn-inline-code") ||
+                                baseToken.type.includes("comment") ||
+                                baseToken.type.includes("katex"))) {
+                                stream.pos += baseToken.size;
+                            } else if (match && match.index === stream.pos) {
+                                // advance
+                                stream.pos += match[0].length || 1;
+                                return 'enhancement-image-size';
+                            } else if (match) {
+                                // jump to the next match
+                                stream.pos = match.index;
+                            } else {
+                                stream.skipToEnd();
+                            }
+
+                            return null;
+                        }
+                    });
                 });
             },
             codeMirrorResources: ['addon/mode/overlay'],
@@ -79,4 +105,9 @@ module.exports = {
             }
         }
     },
+}
+
+function exec(query: RegExp, stream: any) {
+    query.lastIndex = stream.pos;
+    return query.exec(stream.string);
 }
