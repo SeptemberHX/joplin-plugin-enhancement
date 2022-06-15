dae63611c841b3c85f7748ad864c15dddeb0fce2
SeptemberHX <september_hx@outlook.com>
2022-06-15 14:48:01 +0800

    change: better mermaid folder

--- src/driver/codemirror/mermaidFolder/mermaidFolder.ts
+++ src/driver/codemirror/mermaidFolder/mermaidFolder.ts
@@ -14,6 +14,11 @@ export default class MermaidFolder {
         await this.foldAll();
         this.editor.on('cursorActivity', this.unfoldAtCursor.bind(this));
         this.editor.on('cursorActivity', debounce(this.foldAll.bind(this), 200));
+        this.editor.on('change', function (cm, changeObjs) {
+            if (changeObjs.origin === 'setValue') {
+                this.foldAll();
+            }
+        }.bind(this));
     }
 
     private async foldAll() {
@@ -23,53 +28,55 @@ export default class MermaidFolder {
         let meetMermaid = false;
         let from;
         let blockType = 'mermaid';
-        doc.eachLine((line) => {
-            const lineNo = doc.getLineNumber(line);
-            if (lineNo === null) {
-                return;
-            }
+        this.editor.operation(function () {
+            doc.eachLine((line) => {
+                const lineNo = doc.getLineNumber(line);
+                if (lineNo === null) {
+                    return;
+                }
 
-            const lineTokens = this.editor.getLineTokens(lineNo);
+                const lineTokens = this.editor.getLineTokens(lineNo);
 
-            for (const [tokenIndex, token] of lineTokens.entries()) {
-                if (!token.type?.includes('line-cm-jn-code-block')) {
-                    if (!meetMermaid) {
-                        continue;
-                    }
-                } else {
-                    if (!meetMermaid && (token.string.includes('mermaid') || token.string.includes('papers'))) {
-                        meetMermaid = true;
-                        from = {line: lineNo, ch: token.start};
-                        blockType = token.string.includes('mermaid') ? 'mermaid' : 'papers';
-                    } else if (token.string.endsWith('```')) {
-                        if (!meetMermaid) continue;
-                        meetMermaid = false;
-
-                        // not fold when it is folded ?
-                        if (this.editor
-                            .findMarksAt({line: lineNo, ch: token.start})
-                            .find((marker) => marker.className === MARKER_CLASS_NAME)) {
-                            break;
+                for (const [tokenIndex, token] of lineTokens.entries()) {
+                    if (!token.type?.includes('line-cm-jn-code-block')) {
+                        if (!meetMermaid) {
+                            continue;
                         }
+                    } else {
+                        if (!meetMermaid && (token.string.includes('mermaid') || token.string.includes('papers'))) {
+                            meetMermaid = true;
+                            from = {line: lineNo, ch: token.start};
+                            blockType = token.string.includes('mermaid') ? 'mermaid' : 'papers';
+                        } else if (token.string.endsWith('```')) {
+                            if (!meetMermaid) continue;
+                            meetMermaid = false;
 
-                        // not fold when the cursor is in the block
-                        if (cursor.line <= lineNo && cursor.line >= from.line) {
-                            break;
+                            // not fold when it is folded ?
+                            if (this.editor
+                                .findMarksAt({line: lineNo, ch: token.start})
+                                .find((marker) => marker.className === MARKER_CLASS_NAME)) {
+                                break;
+                            }
+
+                            // not fold when the cursor is in the block
+                            if (cursor.line <= lineNo && cursor.line >= from.line) {
+                                break;
+                            }
+
+                            doc.markText(
+                                {line: from.line, ch: 0},
+                                {line: lineNo, ch: token.end},
+                                {
+                                    replacedWith: this.createFoldMarker(token.string, blockType),
+                                    handleMouseEvents: true,
+                                    className: MARKER_CLASS_NAME, // class name is not renderer in DOM
+                                },
+                            );
                         }
-
-                        doc.markText(
-                            { line: from.line, ch: 0 },
-                            { line: lineNo, ch: token.end },
-                            {
-                                replacedWith: this.createFoldMarker(token.string, blockType),
-                                handleMouseEvents: true,
-                                className: MARKER_CLASS_NAME, // class name is not renderer in DOM
-                            },
-                        );
                     }
                 }
-            }
-        });
+            });
+        }.bind(this));
     }
 
     private unfoldAtCursor() {
