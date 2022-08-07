9bffd8ebb573f4eb4b47ca2d7251d78063871311
SeptemberHX <september_hx@outlook.com>
2022-08-08 00:00:53 +0800

    change: link, image, and footnote marker are switching to CMInlineMarkerHelperV2. It should be helpful for video/pdf/xxx rendering in markdown editor

--- src/driver/codemirror/linkFolder/index.ts
+++ src/driver/codemirror/linkFolder/index.ts
@@ -1,4 +1,5 @@
-import CMInlineMarkerHelper from "../../../utils/CMInlineMarkerHelper";
+import CMInlineMarkerHelperV2 from "../../../utils/CMInlineMarkerHelperV2";
+import {debounce} from "ts-debounce";
 
 const ENHANCED_LINK_MARKER = 'enhancement-link-marker';
 const ENHANCED_IMAGE_MARKER = 'enhancement-image-marker';
@@ -30,62 +31,74 @@ module.exports = {
         return {
             plugin: function (CodeMirror) {
                 CodeMirror.defineOption("enhancementLinkFolder", [], async function (cm, val, old) {
-                    new CMInlineMarkerHelper(_context, cm, regexList, function (match, regIndex: number, from, to) {
+                    const linkMarker = new CMInlineMarkerHelperV2(_context, cm, regexList[0], function (match, from, to) {
                         const markEl = document.createElement('span');
-                        if (regIndex === 0) {  // link
-                            markEl.classList.add(ENHANCED_LINK_MARKER);
+                        markEl.classList.add(ENHANCED_LINK_MARKER);
 
-                            if (match[2].startsWith(':/')) {
-                                const joplinIcon = document.createElement('span');
-                                joplinIcon.classList.add(ENHANCED_LINK_MARKER_ICON, 'enhancement-joplin-icon');
-                                markEl.appendChild(joplinIcon);
-                            } else if (match[2].startsWith('#')) {
-                                // do nothing for link to current note
-                            } else {
-                                const iconEl = document.createElement('i');
-                                iconEl.classList.add(ENHANCED_LINK_MARKER_ICON, 'fas', 'fa-link');
-                                markEl.appendChild(iconEl);
-                            }
-
-                            const textEl = document.createElement('a');
-                            textEl.classList.add(ENHANCED_LINK_MARKER_TEXT);
-                            markEl.appendChild(textEl);
-
-                            let regularLinkCaption = match[1];
-                            regularLinkCaption = regularLinkCaption.replace(/\*\*([^*]+?)\*\*/g, '<strong>$1</strong>')
-                            regularLinkCaption = regularLinkCaption.replace(/__([^_]+?)__/g, '<strong>$1</strong>')
-                            regularLinkCaption = regularLinkCaption.replace(/\*([^*]+?)\*/g, '<em>$1</em>')
-                            regularLinkCaption = regularLinkCaption.replace(/\s_([^_]+?)_/g, ' <em>$1</em>')
-                            regularLinkCaption = regularLinkCaption.replace(/^_([^_]+?)_/, '<em>$1</em>')
-                            regularLinkCaption = regularLinkCaption.replace(/~~([^~]+?)~~/g, '<del>$1</del>')
-                            regularLinkCaption = regularLinkCaption.replace(/`([^`]+?)`/g, '<code>$1</code>')
-                            regularLinkCaption = regularLinkCaption.replace(/==([^=]+?)==/g, '<mark>$1</mark>')
-                            regularLinkCaption = regularLinkCaption.replace(/\+\+([^\+]+?)\+\+/g, '<ins>$1</ins>')
-                            textEl.innerHTML = regularLinkCaption;
-                            textEl.title = match[2];
-                        } else if (regIndex === 1) {
-                            markEl.classList.add(ENHANCED_IMAGE_MARKER);
+                        if (match[2].startsWith(':/')) {
+                            const joplinIcon = document.createElement('span');
+                            joplinIcon.classList.add(ENHANCED_LINK_MARKER_ICON, 'enhancement-joplin-icon');
+                            markEl.appendChild(joplinIcon);
+                        } else if (match[2].startsWith('#')) {
+                            // do nothing for link to current note
+                        } else {
                             const iconEl = document.createElement('i');
-                            iconEl.classList.add(ENHANCED_IMAGE_MARKER_ICON, 'fas', 'fa-image');
+                            iconEl.classList.add(ENHANCED_LINK_MARKER_ICON, 'fas', 'fa-link');
                             markEl.appendChild(iconEl);
+                        }
 
-                            const textEl = document.createElement('span');
-                            textEl.classList.add(ENHANCED_IMAGE_MARKER_TEXT);
-                            textEl.textContent = match[1];
-                            markEl.appendChild(textEl);
+                        const textEl = document.createElement('a');
+                        textEl.classList.add(ENHANCED_LINK_MARKER_TEXT);
+                        markEl.appendChild(textEl);
 
-                            if (match[3]) {
-                                const sizeEl = document.createElement('span');
-                                sizeEl.classList.add(ENHANCED_IMAGE_SIZE_TEXT);
-                                sizeEl.textContent = match[3].substr(1, match[3].length - 2);
-                                markEl.appendChild(sizeEl);
+                        let regularLinkCaption = match[1];
+                        regularLinkCaption = regularLinkCaption.replace(/\*\*([^*]+?)\*\*/g, '<strong>$1</strong>')
+                        regularLinkCaption = regularLinkCaption.replace(/__([^_]+?)__/g, '<strong>$1</strong>')
+                        regularLinkCaption = regularLinkCaption.replace(/\*([^*]+?)\*/g, '<em>$1</em>')
+                        regularLinkCaption = regularLinkCaption.replace(/\s_([^_]+?)_/g, ' <em>$1</em>')
+                        regularLinkCaption = regularLinkCaption.replace(/^_([^_]+?)_/, '<em>$1</em>')
+                        regularLinkCaption = regularLinkCaption.replace(/~~([^~]+?)~~/g, '<del>$1</del>')
+                        regularLinkCaption = regularLinkCaption.replace(/`([^`]+?)`/g, '<code>$1</code>')
+                        regularLinkCaption = regularLinkCaption.replace(/==([^=]+?)==/g, '<mark>$1</mark>')
+                        regularLinkCaption = regularLinkCaption.replace(/\+\+([^\+]+?)\+\+/g, '<ins>$1</ins>')
+                        textEl.innerHTML = regularLinkCaption;
+                        textEl.title = match[2];
+
+                        const typesStr = cm.getTokenTypeAt(from);
+                        if (typesStr) {
+                            for (const typeStr of typesStr.split(' ')) {
+                                markEl.classList.add(`cm-${typeStr}`);
                             }
-                        } else if (regIndex === 2) {
-                            markEl.classList.add(ENHANCED_FOOTNOTE_MARKER);
-                            const textEl = document.createElement('span');
-                            textEl.classList.add(ENHANCED_FOOTNOTE_MARKER_TEXT);
-                            textEl.textContent = match[2];
-                            markEl.appendChild(textEl);
+                        }
+                        return markEl;
+                    }, ENHANCED_MARKER_LIST[0], function (line, lineTokens) {
+                        return true;
+                    }, async function (match, e) {
+                        // open url
+                        await _context.postMessage({
+                            type: 'openUrl',
+                            content: match[2]
+                        });
+                    });
+
+                    const imageMarker = new CMInlineMarkerHelperV2(_context, cm, regexList[1], function (match, from, to) {
+                        const markEl = document.createElement('span');
+
+                        markEl.classList.add(ENHANCED_IMAGE_MARKER);
+                        const iconEl = document.createElement('i');
+                        iconEl.classList.add(ENHANCED_IMAGE_MARKER_ICON, 'fas', 'fa-image');
+                        markEl.appendChild(iconEl);
+
+                        const textEl = document.createElement('span');
+                        textEl.classList.add(ENHANCED_IMAGE_MARKER_TEXT);
+                        textEl.textContent = match[1];
+                        markEl.appendChild(textEl);
+
+                        if (match[3]) {
+                            const sizeEl = document.createElement('span');
+                            sizeEl.classList.add(ENHANCED_IMAGE_SIZE_TEXT);
+                            sizeEl.textContent = match[3].substr(1, match[3].length - 2);
+                            markEl.appendChild(sizeEl);
                         }
 
                         const typesStr = cm.getTokenTypeAt(from);
@@ -94,22 +107,46 @@ module.exports = {
                                 markEl.classList.add(`cm-${typeStr}`);
                             }
                         }
-
                         return markEl;
-                    }, ENHANCED_MARKER_LIST, function (line, lineTokens) {
-                        // for (const [tokenIndex, token] of lineTokens.entries()) {
-                        //     if (token.type?.includes('katex')) {
-                        //         return false;
-                        //     }
-                        // }
+                    }, ENHANCED_MARKER_LIST[1], function (line, lineTokens) {
                         return true;
-                    }, async function (match, regIndex, e) {
-                        if (regIndex === 0) {
-                            // open url
-                            await _context.postMessage({
-                                type: 'openUrl',
-                                content: match[2]
-                            });
+                    });
+
+                    const footnoteMarker = new CMInlineMarkerHelperV2(_context, cm, regexList[2], function (match, from, to) {
+                        const markEl = document.createElement('span');
+                        markEl.classList.add(ENHANCED_FOOTNOTE_MARKER);
+                        const textEl = document.createElement('span');
+                        textEl.classList.add(ENHANCED_FOOTNOTE_MARKER_TEXT);
+                        textEl.textContent = match[2];
+                        markEl.appendChild(textEl);
+
+                        const typesStr = cm.getTokenTypeAt(from);
+                        if (typesStr) {
+                            for (const typeStr of typesStr.split(' ')) {
+                                markEl.classList.add(`cm-${typeStr}`);
+                            }
+                        }
+                        return markEl;
+                    }, ENHANCED_MARKER_LIST[2], function (line, lineTokens) {
+                        return true;
+                    });
+
+                    function process(full: boolean) {
+                        cm.startOperation();
+                        linkMarker.process(full);
+                        imageMarker.process(full);
+                        footnoteMarker.process(full);
+                        cm.endOperation();
+                    }
+
+                    const foldDebounce = debounce(() => {
+                        process(true);
+                    }, 100)
+                    cm.on('cursorActivity', foldDebounce);
+                    cm.on('viewportChange', foldDebounce);
+                    cm.on('change', function (cm, changeObjs) {
+                        if (changeObjs.origin === 'setValue') {
+                            process(true);
                         }
                     });
                 });
---
+++ src/utils/CMInlineMarkerHelperV2.ts
@@ -0,0 +1,120 @@
+// implemented according to https://github.com/ylc395/joplin-plugin-note-link-system/blob/main/src/driver/codeMirror/UrlFolder.ts
+
+import { debounce } from "ts-debounce";
+import clickAndClear from "./click-and-clear";
+import {isRangeSelected} from "./cm-utils";
+
+interface MarkerMatch {
+    match;
+}
+
+export default class CMInlineMarkerHelperV2 {
+    renderer: (match, from, to) => any;
+    lineFilter: (line: string, lineToken: []) => boolean;
+    clicked: (match, e: MouseEvent) => void;
+    regex;
+    MARKER_CLASS_NAME: string;
+
+    constructor(private readonly context, private readonly editor, regex, renderer, MARKER_CLASS_NAME, lineFilter?, clicked?) {
+        this.regex = regex;
+        this.renderer = renderer;
+        this.lineFilter = lineFilter;
+        this.clicked = clicked;
+        this.MARKER_CLASS_NAME = MARKER_CLASS_NAME;
+    }
+
+    /**
+     * This function should be in editor.operation()
+     */
+    public process(afterSetValue: boolean = false) {
+        const viewport = this.editor.getViewport()
+        const doc = this.editor.getDoc();
+        const currentCursor = this.editor.getCursor();
+
+        let fromLine = viewport.from;
+        let toLine = viewport.to;
+
+        if (afterSetValue) {
+            fromLine = 0;
+
+            // improve user experience;
+            // todo: decide whether use smaller number or not
+            toLine = this.editor.lineCount();
+        }
+
+        for (let lineNo = fromLine; lineNo < toLine; ++lineNo) {
+            const line = doc.getLine(lineNo);
+            if (!line) {
+                continue;
+            }
+
+            const lineTokens = this.editor.getLineTokens(lineNo);
+            if (this.lineFilter) {
+                if (!this.lineFilter(line, lineTokens)) {
+                    continue;
+                }
+            }
+
+            // to process the situations like ==Hello **World**== correctly
+            //   we need to get all match and process with specific orders
+            let lineMatches: MarkerMatch[] = [];
+            for (const match of line.matchAll(this.regex)) {
+                lineMatches.push({
+                    match: match
+                });
+            }
+
+            // we need to process from inside out, which means we need to process them in reverse order
+            for (let currIndex = lineMatches.length - 1; currIndex >= 0; --currIndex) {
+                const markerMatch = lineMatches[currIndex];
+                this.foldByMatch(doc, lineNo, markerMatch.match);
+            }
+        }
+    }
+
+    private foldByMatch(doc, lineNo, match) {
+        if (match) {
+            const cursor = this.editor.getCursor();
+            // not fold when it is folded ?
+            this.editor.findMarksAt({line: lineNo, ch: match.index}).find((marker) => {
+                if (marker.className === this.MARKER_CLASS_NAME) {
+                    marker.clear();
+                }
+            });
+
+            const from = {line: lineNo, ch: match.index};
+            const to = {line: lineNo, ch: match.index + match[0].length};
+
+            let selected = isRangeSelected(from, to, this.editor);
+
+            if (!selected) {
+                // not fold when the cursor is in the block
+                if (!(cursor.line === lineNo && cursor.ch >= from.ch && cursor.ch <= to.ch)) {
+                    const element = this.renderer(match, from, to);
+                    const textMarker = doc.markText(
+                        from,
+                        to,
+                        {
+                            replacedWith: element,
+                            className: this.MARKER_CLASS_NAME, // class name is not renderer in DOM
+                            clearOnEnter: true,
+                            inclusiveLeft: false,
+                            inclusiveRight: false
+                        },
+                    );
+
+                    element.onclick = (e) => {
+                        if (e.ctrlKey || e.metaKey) {
+                            e.preventDefault();
+                            if (this.clicked) {
+                                this.clicked(match, e);
+                            }
+                        } else {
+                            clickAndClear(textMarker, this.editor)(e);
+                        }
+                    };
+                }
+            }
+        }
+    }
+}
