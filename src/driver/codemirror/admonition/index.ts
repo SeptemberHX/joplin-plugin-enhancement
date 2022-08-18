6b971069fe364cbec2faffbf603a8d37c9d181ac
SeptemberHX <september_hx@outlook.com>
2022-08-18 22:05:06 +0800

    add: line strikethrough for done tasks

--- src/driver/codemirror/admonition/index.ts
+++ src/driver/codemirror/admonition/index.ts
@@ -70,56 +70,37 @@ module.exports = {
                 CodeMirror.defineOption("gfm-joplin-markdown", [], async function(cm, val, old) {
                     cm.setOption('mode', 'gfm-joplin-markdown');
 
-                    cm.addOverlay({
-                        requiredSettings: ['extraCSS'],
-                        token: function (stream: any) {
-                            const match = exec(/(?<=(!\[.*]\(.*\)))(\{.*\})/g, stream);
+                    function addOverlay(cm, reg, className) {
+                        cm.addOverlay({
+                            requiredSettings: ['extraCSS'],
+                            token: function (stream: any) {
+                                const match = exec(reg, stream);
 
-                            const baseToken = stream.baseToken();
-                            if (baseToken?.type && (
-                                baseToken.type.includes("jn-inline-code") ||
-                                baseToken.type.includes("comment") ||
-                                baseToken.type.includes("katex"))) {
-                                stream.pos += baseToken.size;
-                            } else if (match && match.index === stream.pos) {
-                                // advance
-                                stream.pos += match[0].length || 1;
-                                return 'enhancement-image-size';
-                            } else if (match) {
-                                // jump to the next match
-                                stream.pos = match.index;
-                            } else {
-                                stream.skipToEnd();
+                                const baseToken = stream.baseToken();
+                                if (baseToken?.type && (
+                                    baseToken.type.includes("jn-inline-code") ||
+                                    baseToken.type.includes("comment") ||
+                                    baseToken.type.includes("katex"))) {
+                                    stream.pos += baseToken.size;
+                                } else if (match && match.index === stream.pos) {
+                                    // advance
+                                    stream.pos += match[0].length || 1;
+                                    return className;
+                                } else if (match) {
+                                    // jump to the next match
+                                    stream.pos = match.index;
+                                } else {
+                                    stream.skipToEnd();
+                                }
+
+                                return null;
                             }
+                        });
+                    }
 
-                            return null;
-                        }
-                    });
-
-                    cm.addOverlay({
-                        requiredSettings: ['extraCSS'],
-                        token: function (stream: any) {
-                            const match = exec(/(?<!\$)\$(.+?)\$(?!\$)/g, stream);
-
-                            const baseToken = stream.baseToken();
-                            if (baseToken?.type && (
-                                baseToken.type.includes("jn-inline-code") ||
-                                baseToken.type.includes("comment"))) {
-                                stream.pos += baseToken.size;
-                            } else if (match && match.index === stream.pos) {
-                                // advance
-                                stream.pos += match[0].length || 1;
-                                return 'enhancement-katex-inline-math';
-                            } else if (match) {
-                                // jump to the next match
-                                stream.pos = match.index;
-                            } else {
-                                stream.skipToEnd();
-                            }
-
-                            return null;
-                        }
-                    });
+                    addOverlay(cm, /(?<=(!\[.*]\(.*\)))(\{.*\})/g, 'enhancement-image-size');
+                    addOverlay(cm, /(?<!\$)\$(.+?)\$(?!\$)/g, 'enhancement-katex-inline-math');
+                    addOverlay(cm, /- \[[x|X]\]\s+.*/g, 'enhancement-finished-task');
                 });
             },
             codeMirrorResources: ['addon/mode/overlay'],
