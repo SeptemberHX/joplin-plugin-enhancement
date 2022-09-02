23e3dfb5fe1295865d304516de5b7ac00e32c5c4
SeptemberHX <september_hx@outlook.com>
2022-09-02 23:09:13 +0800

    add: inline tag for sentences

--- src/driver/codemirror/inlineMarker/index.ts
+++ src/driver/codemirror/inlineMarker/index.ts
@@ -1,7 +1,10 @@
 import {exec} from "../../../utils/reg";
 
-const markerKeywordReg = /(?<=\()\S+::(?=[^\]]+\))/g;
-const markerPart = /\((\S+)::[^\]]+\)/g;
+const markerTagsReg = /(?<=\()\S+(?=::[^)]+\))/g;
+const markerColons = /(?<=\(\S+)::(?=[^)]+\))/g;
+const markerLeftOpen = /\((?=\S+::[^)]+\))/g;
+const markerRightClose = /(?<=\(\S+::[^)]+)\)/g;
+const markerText = /(?<=\(\S+::)[^)]+(?=\))/g;
 
 module.exports = {
     default: function(_context) {
@@ -12,7 +15,7 @@ module.exports = {
                     cm.addOverlay({
                         requiredSettings: ['extraCSS'],
                         token: function (stream: any) {
-                            const match = exec(markerKeywordReg, stream);
+                            const match = exec(markerTagsReg, stream);
 
                             const baseToken = stream.baseToken();
                             if (baseToken?.type && (
@@ -23,7 +26,7 @@ module.exports = {
                             } else if (match && match.index === stream.pos) {
                                 // advance
                                 stream.pos += match[0].length || 1;
-                                return `editor-marker-keyword editor-marker-keyword-${match[0].substr(0, match[0].length - 2)}`;
+                                return `editor-marker-keyword editor-marker-keyword-${match[0]}`;
                             } else if (match) {
                                 // jump to the next match
                                 stream.pos = match.index;
@@ -38,7 +41,7 @@ module.exports = {
                     cm.addOverlay({
                         requiredSettings: ['extraCSS'],
                         token: function (stream: any) {
-                            const match = exec(markerPart, stream);
+                            const match = exec(markerColons, stream);
 
                             const baseToken = stream.baseToken();
                             if (baseToken?.type && (
@@ -49,7 +52,85 @@ module.exports = {
                             } else if (match && match.index === stream.pos) {
                                 // advance
                                 stream.pos += match[0].length || 1;
-                                return `editor-marker editor-marker-${match[1]}`;
+                                return `editor-marker editor-marker-colons`;
+                            } else if (match) {
+                                // jump to the next match
+                                stream.pos = match.index;
+                            } else {
+                                stream.skipToEnd();
+                            }
+
+                            return null;
+                        }
+                    });
+
+                    cm.addOverlay({
+                        requiredSettings: ['extraCSS'],
+                        token: function (stream: any) {
+                            const match = exec(markerLeftOpen, stream);
+
+                            const baseToken = stream.baseToken();
+                            if (baseToken?.type && (
+                                baseToken.type.includes("jn-inline-code") ||
+                                baseToken.type.includes("comment") ||
+                                baseToken.type.includes("katex"))) {
+                                stream.pos += baseToken.size;
+                            } else if (match && match.index === stream.pos) {
+                                // advance
+                                stream.pos += match[0].length || 1;
+                                return `editor-marker editor-marker-left-open`;
+                            } else if (match) {
+                                // jump to the next match
+                                stream.pos = match.index;
+                            } else {
+                                stream.skipToEnd();
+                            }
+
+                            return null;
+                        }
+                    });
+
+                    cm.addOverlay({
+                        requiredSettings: ['extraCSS'],
+                        token: function (stream: any) {
+                            const match = exec(markerRightClose, stream);
+
+                            const baseToken = stream.baseToken();
+                            if (baseToken?.type && (
+                                baseToken.type.includes("jn-inline-code") ||
+                                baseToken.type.includes("comment") ||
+                                baseToken.type.includes("katex"))) {
+                                stream.pos += baseToken.size;
+                            } else if (match && match.index === stream.pos) {
+                                // advance
+                                stream.pos += match[0].length || 1;
+                                return `editor-marker editor-marker-right-close`;
+                            } else if (match) {
+                                // jump to the next match
+                                stream.pos = match.index;
+                            } else {
+                                stream.skipToEnd();
+                            }
+
+                            return null;
+                        }
+                    });
+
+                    cm.addOverlay({
+                        requiredSettings: ['extraCSS'],
+                        token: function (stream: any) {
+                            const match = exec(markerText, stream);
+
+                            const baseToken = stream.baseToken();
+                            if (baseToken?.type && (
+                                baseToken.type.includes("jn-inline-code") ||
+                                baseToken.type.includes("comment") ||
+                                baseToken.type.includes("katex"))) {
+                                stream.pos += baseToken.size;
+                            } else if (match && match.index === stream.pos) {
+                                // advance
+                                stream.pos += match[0].length || 1;
+                                return `editor-marker editor-marker-text`;
                             } else if (match) {
                                 // jump to the next match
                                 stream.pos = match.index;
