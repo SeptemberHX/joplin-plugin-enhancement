93603dd3e2ce900fc99dc0d18ce59b19f7c1d8ad
Henry <henryuheino@gmail.com>
2024-02-28 19:48:13 -0800

    Revert unnecessary change to index.ts

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,21 @@ joplin.plugins.register({
 	onStart: async function() {
 
 		await settings.register();
+		const enhancementConfig = await getConfig();
+
+		await joplin.contentScripts.register(
+			ContentScriptType.CodeMirrorPlugin,
+			'enhancement_codemirror_items',
+			'./driver/codemirror/index.js'
+		);
+
+		if (enhancementConfig.admonitionCmRender) {
+			await joplin.contentScripts.register(
+				ContentScriptType.CodeMirrorPlugin,
+				'cm_admonition_renderer',
+				'./driver/codemirror/admonition/index.js'
+			);
+		}
 
 		await joplin.contentScripts.onMessage(
 			'enhancement_codemirror_items',
@@ -78,22 +93,6 @@ joplin.plugins.register({
 				}
 			}
 		);
-
-		const enhancementConfig = await getConfig();
-
-		await joplin.contentScripts.register(
-			ContentScriptType.CodeMirrorPlugin,
-			'enhancement_codemirror_items',
-			'./driver/codemirror/index.js'
-		);
-
-		if (enhancementConfig.admonitionCmRender) {
-			await joplin.contentScripts.register(
-				ContentScriptType.CodeMirrorPlugin,
-				'cm_admonition_renderer',
-				'./driver/codemirror/admonition/index.js'
-			);
-		}
 
 		if (enhancementConfig.imageEnhancement) {
 			await joplin.contentScripts.register(
